4e5741816e01b4c16631290c92d8a59c64d574d1
krandder <azsantos.k@gmail.com>
2025-05-19 22:20:11 -0300

    test: add smoke test for FutarchyOracle

---
+++ test/FutarchyOracle.t.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "forge-std/Test.sol";
+
+contract FutarchyOracleTest is Test {
+    function test_SmokeTest() public {
+        assertTrue(true, "Basic smoke test passes");
+    }
+}
